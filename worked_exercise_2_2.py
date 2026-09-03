96091264236c101f27fa8c39bcd1603c7672cf9f
unknown <https://github.com/officexcoder>
2026-09-03 07:38:24 -0700

    first_commit_submission

---
+++ worked_exercise_2_2.py
@@ -0,0 +1,9 @@
+
+def print_hi(name):
+    print(f'Hi, {name}')
+name = input("Enter your name: ")
+print("Hello", name)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
---
+++ worked_exercise_2_3.py
@@ -0,0 +1,7 @@
+
+hours_worked = float(input("Enter Hours: "))
+hourly_rate = float(input("Enter Rate: "))
+
+gross_pay = hours_worked * hourly_rate
+
+print("Pay:", gross_pay)
