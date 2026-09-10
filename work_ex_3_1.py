8b7d41cb9f226c5242335d2dd319760f1f7ed1f0
unknown <https://github.com/officexcoder>
2026-09-10 11:09:12 -0700

    work_ex_3_1 adjusted

--- work_ex_3_1.py
+++ work_ex_3_1.py
@@ -4,9 +4,16 @@ fh = float(sh)
 fr = float(sr)
 
 # print(fh, fr)
+
+
+
 if fh > 40 :
     print("Overtime")
+    reg = fr * fh
+    otp = (fh - 40.0) * (fr * 0.5)
+    print(reg,otp)
+    xp = reg + otp
 else:
     print("Regular")
-xp = fh * fr
+    xp = fh * fr
 print("Pay:",xp)
